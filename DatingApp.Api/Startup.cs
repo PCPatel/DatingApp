f81e6b2474ff3dfbd481c4275121399fdce624ed
PCPatel <patelpc.mits@gmail.com>
2019-12-24 21:59:52 +0530

    End of section 13

--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -61,6 +61,7 @@ namespace DatingApp.Api
                 });
 
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
